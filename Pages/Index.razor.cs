91d3963bce4f7eef22572d9a2c302893e2924f25
Duje Vatavuk <duje@vatavuk.com>
2024-03-08 11:22:40 +0100

    added monthly statistics to home page

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.JSInterop;
 using Radzen;
 using Radzen.Blazor;
@@ -34,5 +35,46 @@ namespace CRMBlazorServerRBS.Pages
 
         [Inject]
         protected SecurityService Security { get; set; }
+
+        [Inject]
+        public Data.RadzenCRMContext Context { get; set; }
+
+        public Stats MonthlyStats()
+        {
+            double wonOpportunities = Context.Opportunities
+                                .Include(opportunity => opportunity.OpportunityStatus)
+                                .Where(opportunity => opportunity.OpportunityStatus.Name == "Won")
+                                .Count();
+
+            var totalOpportunities = Context.Opportunities.Count();
+
+            var ratio = wonOpportunities / totalOpportunities;
+
+            return Context.Opportunities
+                        .Include(opportunity => opportunity.OpportunityStatus)
+                        .Where(opportunity => opportunity.OpportunityStatus.Name == "Won")
+                        .ToList()
+                        .GroupBy(opportunity => new DateTime(opportunity.CloseDate.Year, opportunity.CloseDate.Month, 1))
+                        .Select(group => new Stats()
+                        {
+                            Month = group.Key,
+                            Revenue = group.Sum(opportunity => opportunity.Amount),
+                            Opportunities = group.Count(),
+                            AverageDealSize = group.Average(opportunity => opportunity.Amount),
+                            Ratio = ratio
+                        })
+                        .OrderBy(deals => deals.Month)
+                        .LastOrDefault();
+        }
     }
+}
+
+public class Stats
+{
+    public DateTime Month { get; set; }
+    public decimal Revenue { get; set; }
+
+    public int Opportunities { get; set; }
+    public decimal AverageDealSize { get; set; }
+    public double Ratio { get; set; }
 }
